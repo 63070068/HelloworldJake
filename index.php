d9a7108e534e78bd311ec49ad2b39a51567f5603
Tiwat Parkpoompaisal <73679296+63070068@users.noreply.github.com>
2020-10-30 15:02:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1>Jake Advengers time</h1>
+    <p>Jake is dog</p>
+    <img src="jake.jpg">
+    <br>
+    <a href="http://www.it.kmitl.ac.th">This is Link</a>
+
+    <h2>Love Jake</h2>
+    <ul>
+        <li>Google</li>
+        <li>Yahoo</li>
+        <il>Amazon</il>
+    </ul>
+
+</body>
+
+</html>
