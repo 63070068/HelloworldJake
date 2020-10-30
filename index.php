e8d54fd69ce18674a861190aec5b96a8fbe5f744
Tiwat Parkpoompaisal <73679296+63070068@users.noreply.github.com>
2020-10-30 15:55:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Resume</title>
+    <style type="text/css">
+    body {
+        background-color: #00bda7;
+        color: #08c986;
+    }
+    </style>
 </head>
 
 <body>
-    <h1>Tiwat Parkpoompaisal</h1>
-    <h2>Nick name: Jean</h2>
-    <img src="meme.jpg" width="500px">
+    <h1 align="center">Tiwat Parkpoompaisal</h1>
+    <h2 align="center">Nick name: Jean</h2>
+    <img src="meme.jpg">
     <p>กาลครั้งหนึ่งนานมาแล้ว เมื่อตอนผมเรียน ประถมศึกษาปีที่ 2</p>
     <p>โรงเรียนประถมของผมมีวิชา ตีกอล์ฟ ก่อนที่อาจารย์จะเริ่มสอนนั้น</p>
     <p>เพื่อนของผมบอกผมว่าปวดขี้ แต่ผมไม่เชื่อเลยรีบวิ่งตามเพื่อนไปแล้วเตะตูดเพื่อน</p>
     <p>แต่เนื่องจากเพื่อนปวดมาก พอโดนเท้ากระทบอย่างแรง</p>
-    <p>จึงกลั้นไม่อยู่เลอะเต็มกางเกงจนไหลลงมาโดนหญ้าในสนามกอล์ฟเลยครับ5555555555555555555555555555555+</p>
+    <p>จึงกลั้นไม่อยู่เลอะเต็มกางเกงจนไหลลงมาโดนหญ้าในสนามกอล์ฟเลยครับ555555555555555555555555555555+</p>
     <br>
     <a href="https://github.com">This is Link of GITHUB</a>
     <a href="https://portal.azure.com">This is Link of Azure</a>
+    <h2>Love Jake</h2>
     <ul>
         <li>Google</li>
         <li>Yahoo</li>
